ee74f5b3b033f501e7891a7dcaed6fe499da726f
MAlex <alexmaximovj@gmail.com>
2015-06-13 21:51:58 +0300

    add recursion

--- src/task_00_exchange_variable/Enum_Descrease_Array.java
+++ src/task_00_exchange_variable/Enum_Descrease_Array.java
@@ -7,7 +7,7 @@ public class Enum_Descrease_Array {
 
     public static void main(String[] args) {
         int[] array = {1, 2, 3, 4, 5, 6, 7, 8};
-        for (int i = array.length - 1; i > 1; i--)
+        for (int i = array.length - 1; i >=0; i--)
             System.out.print(array[i] + " ");
 
     }
---
+++ src/task_recursion/Example_02.java
@@ -0,0 +1,12 @@
+package task_recursion;
+
+/**
+ * Created by Alex on 13.06.2015.
+ */
+public class Example_02 {
+
+    public static void main(String[] args) {
+
+        
+    }
+}
